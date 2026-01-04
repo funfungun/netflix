58b43c503c163ea4c7d601a5a3d22acf7632ed2c
funfun <alpha5179@naver.com>
2026-01-04 18:22:59 +0900

    feat: Custom Validator

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,40 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({
+  async: true,
+})
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    // 비밀번호 길이는 4-8
+    return value.length > 4 && value.length < 8;
+  }
+
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return '비밀번호의 길이는 4-8자';
+  }
+}
+
+function IsPasswordValid(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: PasswordValidator,
+    });
+  };
+}
 
 export class UpdateMovieDto {
   @IsNotEmpty()
@@ -8,4 +44,9 @@ export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
   genre?: string;
+
+  @IsPasswordValid({
+    message: 'Other message',
+  })
+  test: string;
 }
